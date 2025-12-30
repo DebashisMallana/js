f71e1e15b176b2d600621dac8009886e1978fd79
DebashisMallana <debashismallana12@gmail.com>
2025-12-30 16:03:53 +0000

    basics of variables in js

---
+++ variables.js
@@ -0,0 +1,6 @@
+let accountName = "Ram"
+let accountEmail = "Ram12@gmail.com"
+// there are three types of ways to declare a variable , let , const and var , let gives a value to a variable and its value can be changed inside the program
+// const changing its value results in an error , var is usually not prefered , cause js earlier failed to identify the scope 
+// also you can give a semi colon or not , not an issue
+console.table([accountName, accountEmail])
